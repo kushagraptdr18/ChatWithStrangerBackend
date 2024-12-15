df4798dc88db9643ca2dd075e55c54f9487d32eb
Kushagra Patidar <kushagraptdr18@gmail.com>
2024-12-15 10:16:28 +0530

    env created with port variable

--- app.js
+++ app.js
@@ -179,6 +179,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+let port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
